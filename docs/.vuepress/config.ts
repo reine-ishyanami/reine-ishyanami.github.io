caaa614001642d572eec132020dc4f0f19404df5
reine-ishyanami <2402979195@qq.com>
2024-07-03 13:00:22 +0800

    add bulletin

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,6 +13,7 @@ export default defineUserConfig({
     theme: recoTheme({
         style: "@vuepress-reco/style-default",
         author: "reine-ishyanami",
+        repo: 'reine-ishyanami/article',
         authorAvatar: "https://avatars.githubusercontent.com/u/46278371?v=4",
         docsRepo: "https://github.com/reine-ishyanami/article",
         docsBranch: "master",
@@ -21,6 +22,15 @@ export default defineUserConfig({
             { text: "Home", link: "/" },
             { text: "Categories", link: "/categories/dev/1/" },
             { text: "Tags", link: "/tags/Console/1/" }
-        ]
-    }),
+        ],
+        bulletin: {
+            body: [
+                {
+                    type: 'text',
+                    content: '如果文章教程中有误，欢迎进入右上方Github地址，在Issues中提出。',
+                    style: 'font-size: 12px;'
+                }
+            ]
+        }
+    })
 })
